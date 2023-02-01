42e8449450c30761600f250f9cc53256aa415802
María Puchades <100128850+emepuchades@users.noreply.github.com>
2023-02-02 00:34:36 +0100

    Added button loader

--- frontend/src/components/UserTwitter.jsx
+++ frontend/src/components/UserTwitter.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import ClassifyIA from './ClassifyIA';
 import axios from 'axios';
+import UserInput from './userInput';
+import Faq from './Faq';
 
 function UserTwitter({ userTwitter, setUserTwitter }) {
     const [timeline, setTimeline] = useState([])
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
 
+
+    useEffect(() => {
+        error !== '' ?
+            setError('')
+            : null
+    }, [userTwitter]);
+
     async function updateTimelines() {
-        if (timeline.length > 0) { setTimeline([]) } else { setError('')}
-        
+        if (timeline.length > 0) { setTimeline([]) } else { setError('') }
+        setLoading(true)
+
         await axios
             .get(`${import.meta.env.PUBLIC_URL_BACKEND}/api/timeline`, {
                 params: {
@@ -24,39 +34,30 @@ function UserTwitter({ userTwitter, setUserTwitter }) {
             })
             .catch(error => console.log('error react', error.message))
 
-        setTimeout(() => {
-            setLoading(true)
-        }, 1000);
+        setLoading(false)
     }
 
     return (
-        <div>
-            <div>
-                <h1>Introduce tu usuario de twitter</h1>
-                <input
-                    type="text"
-                    value={userTwitter}
-                    onChange={(e) => setUserTwitter(e.target.value)}
-                    placeholder='introduce el usuario de tu twitter'
-                />
-                <button
-                    type="button"
-                    onClick={() => updateTimelines()}
-                    className="text-white bg-gradient-to-br from-green-400 to-purple-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                    Ver
-                </button>
-            </div>
-            {timeline.length > 0 || error !== '' ?
-                loading ?
+        <div className="container-dashboard">
+            <UserInput
+                userTwitter={userTwitter}
+                setUserTwitter={setUserTwitter}
+                error={error}
+                loading={loading}
+                updateTimelines={updateTimelines}
+            />
+            {timeline.length > 0 ?
+                !loading ?
                     <div>
                         <ClassifyIA
                             timeline={timeline}
                             error={error}
                         />
-                    </div>  
-                    : <p>Loading</p>
+                    </div>
+                    : null
                 : null}
 
+            <Faq />
         </div>
     )
 }
